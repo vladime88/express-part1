34f82d6446e0375b5b730617eaae10f2a124a46b
vladime88 <vladime@gmail.com>
2020-09-30 17:16:25 +0200

    ex05

---
+++ src/app.js
@@ -0,0 +1,42 @@
+import express from 'express'
+
+const app = express()
+const IP_LOOPBACK = 'localhost'
+const IP_LOCAL = '192.168.1.13'
+const PORT = 7777
+
+app.get('/', (req, res) => {
+    res.send('Exercices express partie 1')
+})
+
+app.get('/aboutme/:hostId', (req, res) => {
+    res.send(`my name is ${req.params.hostId}`)
+})
+
+app.get('/aboutyou', (req, res) => {
+    res.send(`Welcome ${req.ip} to my first express app.`)
+    let head = JSON.stringify(req.headers)
+    res.send(`La version de votre navigateur est ${head['user-agent']}`)
+})
+
+app.get('/vote/:age', (req, res) => {
+    let age = Number(req.params.age)
+    if (age < 18) {
+        res.send(`Vous êtes trop jeune pour voter`)
+    } else {
+        res.send(`Vous pouvez voter!`)
+    }
+})
+
+app.get('/palindrome/:pal', (req, res) => {
+    let pal = req.params.pal
+    if (pal === pal.split('').reverse().join('')) {
+        res.send(`${pal} is a palindrome`)
+    } else {
+        res.send(`${pal} is not a palindrome`)
+    }
+})
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`)
+})
